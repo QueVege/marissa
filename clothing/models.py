438dd7432d7662a4873d0bb047b77c00d6915d32
Valeria Berdnikova <likespony@gmail.com>
2020-01-15 23:23:04 +0200

    add model Product

--- clothing/models.py
+++ clothing/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     url = models.URLField(max_length=200)
     name = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.URLField()
     brand = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
     price = models.FloatField()
